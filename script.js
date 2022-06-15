fce692ec191ded7c52edcc4c9ef91417e250fb3c
fabianairc <fabianaramirezzz@gmail.com>
2022-06-14 21:53:40 -0300

    dia3

---
+++ script.js
@@ -0,0 +1,31 @@
+//now = new Date();
+//localtime = now.toString();
+//utctime = now.toGMTString();
+//document.write("<p><strong>Local time:</strong> " + localtime + "</p>");
+//document.write("<p><strong>UTC time:</strong> " + utctime + "</p>");
+
+//let miVariable = 2;
+
+//console.log("true");
+//function saludar() {
+// return "hola";
+//}
+//console.log(saludar());
+
+//let miVerdadero = true;
+
+//if (miVerdadero === true) {
+//console.log("verdaero");
+//} else {
+//  console.log("falso");
+//}
+//alert("hello world ");
+
+//COMO ES PROMPT
+//let Nombre = prompt("ingresa tu nombre");
+//console.log(Nombre);
+
+//El confirm
+//confirm("are u sure?");
+//o
+//console.log(confirm("are us ure?"));
